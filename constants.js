3a4515ee0bfab742e9d4bc05ad37ea5985c87231
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2025-03-02 16:51:55 -0700

    .

--- constants.js
+++ constants.js
@@ -183,7 +183,7 @@ export const twitterAccounts = [
     username: "SimonKaggwaNjal",
     name: "Simon Kaggwa Njala",
     buyAnyPostedCA: true,
-    amountToBuyForAnyPostedCA: 10,
+    amountToBuyForAnyPostedCA: 5,
     slippageBpsForAnyPostedCA: 4000,
     timeToSellForAnyPostedCA: 120 * 1000,
     priorityFeeForAnyPostedCA: 0.1,
